9882c8a4dadb749a624ef2414ae9e5d76a29fad3
James Clarke <james@clarkezone.net>
2020-02-08 10:45:24 -0800

    use flags to determine what to do

--- main.go
+++ main.go
@@ -1,65 +1,103 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
+
+	"github.com/phayes/hookserve/hookserve"
+)
+
+var (
+	cloneFlag   = flag.Bool("clone", false, "Perform initial clone")
+	monitorFlag = flag.Bool("monitor", false, "Monitor for changes via webhook and pull")
 )
 
 func main() {
-	clone()
-	// server := hookserve.NewServer()
-	// server.Port = 8080
-	// server.Secret = "onetwothree"
-	// //gitLocalDir: /sourcetmp
-	// //targetBranch: acme-ngrok
-	// //gitrepo: gitRepo: https://github.com/clarkezone/clarkezone.github.io.git
-	// server.GoListenAndServe()
+	flag.Parse()
 
-	// // Everytime the server receives a webhook event, print the results
-	// for event := range server.Events {
-	// 	fmt.Println(event.Owner + " " + event.Repo + " " + event.Branch + " " + event.Commit)
-	// }
+	repo := "https://github.com/clarkezone/clarkezone.github.io.git"
+	branch := "acme-ngrok"
+	//localdir := "/srv/jekyll/source"
+	localdir := "clarkezone.github.io"
+	secret := "onetwothree"
+
+	if *cloneFlag {
+		fmt.Printf("Initial clone for\n repo: %v\n branch: %v\n local dir:%v\n", repo, branch, localdir)
+
+		err := clone(repo, branch, localdir)
+		if err != nil {
+			fmt.Printf("Error in initial clone: %v\n", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Done.")
+	}
+
+	if *monitorFlag {
+		fmt.Printf("Monitoring branch: %v", branch)
+		monitor(secret, branch)
+	}
 }
 
-func clone() {
-	os.Mkdir("/srv/jekyll/source", os.ModePerm)
-	os.Mkdir("/app/_site", os.ModePerm)
+func monitor(secret string, branch string) {
+	server := hookserve.NewServer()
+	server.Port = 8080
+	server.Secret = secret
+	//gitLocalDir: /sourcetmp
+	//targetBranch: acme-ngrok
+	//gitrepo: gitRepo: https://github.com/clarkezone/clarkezone.github.io.git
+	server.GoListenAndServe()
 
-	cmd := exec.Command("git", "clone", "https://github.com/clarkezone/clarkezone.github.io.git", ".")
-	cmd.Dir = "/srv/jekyll/source"
+	// Everytime the server receives a webhook event, print the results
+	for event := range server.Events {
+		fmt.Println(event.Owner + " " + event.Repo + " " + event.Branch + " " + event.Commit)
+		if event.Branch == branch {
+			fmt.Println("Match")
+		}
+	}
+}
+
+func clone(repo string, branch string, localfolder string) error {
+	os.Mkdir(localfolder, os.ModePerm)
+
+	cmd := exec.Command("git", "clone", repo, ".")
+	cmd.Dir = localfolder
 	err := cmd.Run()
 
 	if err != nil {
-		fmt.Println("an error occurred.\n")
-		log.Fatal(err)
+		return err
 	}
 
-	//os.Chown("/srv/jekyll/source", 1000, 1000)
+	//os.Chown("/srv/jekyll/source", 1000, 1000) not recursive
 
-	cmd = exec.Command("git", "checkout", "acme-ngrok")
-	cmd.Dir = "/srv/jekyll/source"
+	cmd = exec.Command("git", "checkout", branch)
+	cmd.Dir = localfolder
 	err = cmd.Run()
 
 	if err != nil {
-		fmt.Println("an error occurred.\n")
-		log.Fatal(err)
+		return err
 	}
 
-	cmd = exec.Command("chown", "-R", "jekyll:jekyll", "/srv/jekyll/source")
+	return nil
+}
+
+func prepJekyll(localfolder string, sitdir string) error {
+	os.Mkdir(sitdir, os.ModePerm)
+
+	cmd := exec.Command("chown", "-R", "jekyll:jekyll", localfolder)
+	err := cmd.Run()
+
+	if err != nil {
+		return err
+	}
+
+	cmd = exec.Command("chown", "-R", "jekyll:jekyll", sitdir)
 	err = cmd.Run()
 
 	if err != nil {
-		fmt.Println("an error occurred.\n")
-		log.Fatal(err)
+		return err
 	}
 
-	cmd = exec.Command("chown", "-R", "jekyll:jekyll", "/app/_site")
-	err = cmd.Run()
-
-	if err != nil {
-		fmt.Println("an error occurred.\n")
-		log.Fatal(err)
-	}
+	return nil
 }
