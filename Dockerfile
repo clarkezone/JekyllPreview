8211987e0fe37de67b65f0b719c464097738f887
James Clarke <james@clarkezone.net>
2020-02-08 10:45:24 -0800

    Updates

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM golang:alpine as builder
+RUN mkdir /build
+ADD . /build
+WORKDIR /build
+RUN go build
+
+FROM jekyll/jekyll
+USER root
+RUN mkdir /app
+COPY --from=builder /build/JekyllBlogPreview /app/.
+WORKDIR /app
+ADD startjek.sh .
+RUN chmod +x startjek.sh
+ENV JEKPREV_LOCALDIR=/srv/jekyll/source
+ENV JEKPREV_monitorCmd=/app/startjek.sh
+
+#Use these if you want to fork or customize or not use docker compose
+#env JEKPREV_REPO=<YOUR REPO>
+#env JEKPREV_SECRET=<YOUR SECRET>
+
+ENTRYPOINT [ "/app/JekyllBlogPreview" ]
+
+#to run manually from 
+#docker run --rm -it -p=4000:4000 -p=80:8080 clarkezone/jekpreview
--- main.go
+++ main.go
@@ -2,52 +2,192 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
+	"os/signal"
+
+	"github.com/clarkezone/go-execobservable"
+	"github.com/phayes/hookserve/hookserve"
 )
 
+const (
+	reponame       = "JEKPREV_REPO"
+	secretname     = "JEKPREV_SECRET"
+	localdirname   = "JEKPREV_LOCALDIR"
+	monitorcmdname = "JEKPREV_monitorCmd"
+)
+
+type cleanupfunc func()
+
 func main() {
-	clone()
+	repo, localdir, secret, monitorcmdline := readEnv()
+
+	if repo == "" {
+		fmt.Printf("Repo must be provided in %v\n", reponame)
+		os.Exit(1)
+	}
+
+	if secret == "" {
+		fmt.Printf("Repo must be provided in %v\n", secretname)
+		os.Exit(1)
+	}
+
+	if localdir == "" {
+		fmt.Printf("Localdir be provided in %v\n", localdirname)
+		os.Exit(1)
+	}
+
+	cleanupDone := handleSig(func() { os.RemoveAll(localdir) })
+
+	fmt.Printf("Initial clone for\n repo: %v\n local dir:%v\n", repo, localdir)
+
+	err := clone(repo, localdir)
+	if err != nil {
+		fmt.Printf("Error in initial clone: %v\n", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Done.")
+
+	go func() {
+		fmt.Printf("Monitoring started\n")
+		err := monitor(secret, localdir)
+		if err != nil {
+			fmt.Printf("Monitor failed: %v\n", err.Error())
+			os.Exit(1)
+		}
+	}()
+
+	if monitorcmdline != "" {
+		fmt.Printf("Running commandline %v\n", monitorcmdline)
+		err := prepJekyll(localdir, "_site")
+		if err != nil {
+			fmt.Printf("PrepJekyll failed: %v\n", err.Error())
+			os.Exit(1)
+		}
+		err = runJekyllScript(monitorcmdline)
+		if err != nil {
+			fmt.Printf("Monitor cmdline failed: %v\n", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	<-cleanupDone
 }
 
-func clone() {
-	os.Mkdir("/srv/jekyll/source", os.ModePerm)
-	os.Mkdir("/app/_site", os.ModePerm)
+func handleSig(cleanupwork cleanupfunc) chan struct{} {
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan struct{})
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+		<-signalChan
+		fmt.Printf("\nReceived an interrupt, stopping services...\n")
+		if cleanupwork != nil {
+			cleanupwork()
+		}
 
-	cmd := exec.Command("git", "clone", "https://github.com/clarkezone/clarkezone.github.io.git", ".")
-	cmd.Dir = "/srv/jekyll/source"
+		close(cleanupDone)
+	}()
+	return cleanupDone
+}
+
+func readEnv() (string, string, string, string) {
+	repo := os.Getenv(reponame)
+	localdr := os.Getenv(localdirname)
+	secret := os.Getenv(secretname)
+	monitorcmdline := os.Getenv(monitorcmdname)
+	return repo, localdr, secret, monitorcmdline
+}
+
+func monitor(secret string, localfolder string) error {
+	currentBranch := ""
+	server := hookserve.NewServer()
+	server.Port = 8080
+	server.Secret = secret
+	server.GoListenAndServe()
+
+	// Everytime the server receives a webhook event, print the results
+	for event := range server.Events {
+		fmt.Println(event.Owner + " " + event.Repo + " " + event.Branch + " " + event.Commit)
+
+		if event.Branch != currentBranch {
+			fmt.Printf("Fetching\n")
+			cmd := exec.Command("git", "fetch")
+			cmd.Dir = localfolder
+			err := cmd.Run()
+
+			if err != nil {
+				fmt.Printf("Fetch failed %v\n", err.Error())
+				return err
+			}
+
+			currentBranch = event.Branch
+			fmt.Printf("Checking out new branch %v\n", currentBranch)
+			cmd = exec.Command("git", "checkout", currentBranch)
+			cmd.Dir = localfolder
+			err = cmd.Run()
+
+			if err != nil {
+				fmt.Printf("Checkout new branch failed %v\n", err.Error())
+				return err
+			}
+		}
+
+		fmt.Printf("Pull branch: %v\n", event.Branch)
+		cmd := exec.Command("git", "pull")
+		cmd.Dir = localfolder
+		err := cmd.Run()
+
+		if err != nil {
+			fmt.Printf("Pull failed %v\n", err.Error())
+			return err
+		}
+
+	}
+	return nil
+}
+
+func clone(repo string, localfolder string) error {
+	os.RemoveAll(localfolder) // ignore error since it may not exist
+
+	err := os.Mkdir(localfolder, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("git", "clone", repo, ".")
+	cmd.Dir = localfolder
+	err = cmd.Run()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func prepJekyll(localfolder string, sitdir string) error {
+	cmd := exec.Command("chown", "-R", "jekyll:jekyll", localfolder)
 	err := cmd.Run()
 
 	if err != nil {
-		fmt.Println("an error occurred.\n")
-		log.Fatal(err)
+		return err
 	}
 
-	//os.Chown("/srv/jekyll/source", 1000, 1000)
+	return nil
+}
 
-	cmd = exec.Command("git", "checkout", "acme-ngrok")
-	cmd.Dir = "/srv/jekyll/source"
-	err = cmd.Run()
+type outputprogress struct {
+}
 
-	if err != nil {
-		fmt.Println("an error occurred.\n")
-		log.Fatal(err)
-	}
+func (t outputprogress) Progress(s string, sr execobservable.SendResponse) {
+	fmt.Printf("%v", s)
+}
 
-	cmd = exec.Command("chown", "-R", "jekyll:jekyll", "/srv/jekyll/source")
-	err = cmd.Run()
+func runJekyllScript(cmdstring string) error {
+	output := &outputprogress{}
 
-	if err != nil {
-		fmt.Println("an error occurred.\n")
-		log.Fatal(err)
-	}
+	runner := &execobservable.CmdRunner{}
+	runner.RunCommand("sh", output, "-c", cmdstring)
 
-	cmd = exec.Command("chown", "-R", "jekyll:jekyll", "/app/_site")
-	err = cmd.Run()
-
-	if err != nil {
-		fmt.Println("an error occurred.\n")
-		log.Fatal(err)
-	}
+	return nil
 }
