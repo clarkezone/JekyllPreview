4efb723c40920542e7fe92584b1cc26afc662a46
James Clarke <james@clarkezone.net>
2020-05-19 20:39:44 -0700

    Add basic auth

--- httpshare.go
+++ httpshare.go
@@ -21,11 +21,38 @@ func (man *httpShareManager) shareBranch(branchName string, dir string) {
 	_, ok := man.shares[httpBranchName]
 
 	if !ok {
-		http.Handle(httpBranchName, http.StripPrefix(httpBranchName, http.FileServer(http.Dir(dir))))
+		//http.Handle(httpBranchName, http.StripPrefix(httpBranchName, http.FileServer(http.Dir(dir))))
+		http.Handle(httpBranchName, auth(handleFileServer(dir, httpBranchName)))
 		man.shares[httpBranchName] = dir
 	}
 }
 
 func (man *httpShareManager) NewBranch(branchName string, dir string) {
 	man.shareBranch(branchName, dir)
+}
+
+func handleFileServer(dir, prefix string) http.HandlerFunc {
+	fs := http.FileServer(http.Dir(dir))
+	realHandler := http.StripPrefix(prefix, fs).ServeHTTP
+	return func(w http.ResponseWriter, req *http.Request) {
+		realHandler(w, req)
+	}
+}
+
+func auth(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, pass, _ := r.BasicAuth()
+		if !check(user, pass) {
+			http.Error(w, "Unauthorized.", 401)
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func check(u, p string) bool {
+	if u == "" && p == "" {
+		return true
+	}
+	return false
 }
