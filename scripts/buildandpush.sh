6e8350b1c158a98538ded95636c0b9903390be79
James Clarke <james@clarkezone.net>
2021-12-29 20:07:52 -0800

    push to local CR

--- scripts/buildandpush.sh
+++ scripts/buildandpush.sh
@@ -1,2 +1,4 @@
-podman build . -t ghcr.io/clarkezone/jekyllblogpreview:latest
-podman push ghcr.io/clarkezone/jekyllblogpreview:latest
+#podman build . -t ghcr.io/clarkezone/jekyllblogpreview:latest
+#podman push ghcr.io/clarkezone/jekyllblogpreview:latest
+podman build . -t registry.dev.clarkezone.dev/jekyllblogpreview:latest
+podman push registry.dev.clarkezone.dev/jekyllblogpreview:latest
