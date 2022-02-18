7781a7cbb5658a41c55af9391f8695f48b4313f2
James Clarke <james@clarkezone.net>
2022-02-18 17:17:05 +0000

    Fix local build scripts

--- Preview/scripts/buildpush-local.sh
+++ Preview/scripts/buildpush-local.sh
@@ -5,13 +5,13 @@ export VERSION=$(git describe --abbrev=0)
 echo ${IMG}
 echo ${VERSION}
 
-podman build --arch=amd64 -t ${IMG}:${VERSION}.amd64 -f Dockerfile
-podman build --arch=arm64 -t ${IMG}:${VERSION}.arm64 -f Dockerfile
+#podman build --arch=amd64 -t ${IMG}:${VERSION}.amd64 -f Dockerfile
+#podman build --arch=arm64 -t ${IMG}:${VERSION}.arm64 -f Dockerfile
 
-podman manifest create ${IMG}:${VERSION}
-podman manifest add ${IMG}:${VERSION} containers-storage:localhost/${IMG}:${VERSION}.amd64
-podman manifest add ${IMG}:${VERSION} containers-storage:localhost/${IMG}:${VERSION}.arm64
+#podman manifest create ${IMG}:${VERSION}
+#podman manifest add ${IMG}:${VERSION} containers-storage:localhost/${IMG}:${VERSION}.amd64
+#podman manifest add ${IMG}:${VERSION} containers-storage:localhost/${IMG}:${VERSION}.arm64
 
-podman manifest push ${IMG}:${VERSION} docker://registry.dev.clarkezone.dev${IMG}:${VERSION}
+podman manifest push ${IMG}:${VERSION} docker://registry.dev.clarkezone.dev/${IMG}:${VERSION}
 
 echo podman search registry.dev.clarkezone.dev/${IMG} --list-tags
