f070d2e9e46814cb8254c2d58a256576813a45ce
James Clarke <james@clarkezone.net>
2022-02-18 17:09:11 +0000

    Fix scripts

--- Preview/scripts/buildandpush.sh
+++ Preview/scripts/buildandpush.sh
@@ -1,7 +1,5 @@
-#podman build . -t ghcr.io/clarkezone/jekyllblogpreview:latest
-#podman push ghcr.io/clarkezone/jekyllblogpreview:latest
+#set -x
 export IMG=previewd
-#export VERSION=0.0.1.0
 export VERSION=$(git describe --abbrev=0)
 
 echo ${IMG}
@@ -14,5 +12,8 @@ podman manifest create ${IMG}:${VERSION}
 podman manifest add ${IMG}:${VERSION} containers-storage:localhost/${IMG}:${VERSION}.amd64
 podman manifest add ${IMG}:${VERSION} containers-storage:localhost/${IMG}:${VERSION}.arm64
 
-podman manifest push ${IMG}:{$VERSION} docker:/registry.hub.docker.com/clarkezone/${IMG}:{$VERSION}
-podman manifest push ${IMG}:{$VERSION} docker:/ghcr.io/clarkezone${IMG}:{$VERSION}
+podman manifest push ${IMG}:${VERSION} docker://registry.hub.docker.com/clarkezone/${IMG}:${VERSION}
+podman manifest push ${IMG}:${VERSION} docker://ghcr.io/clarkezone/${IMG}:${VERSION}
+
+echo podman search registry.hub.docker.com/clarkezone/${IMG} --list-tags
+echo podman search ghcr.io/clarkezone/${IMG} --list-tags
