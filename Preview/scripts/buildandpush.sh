0ab17e479b45bebef9219b0ab3369e060c67058d
James Clarke <james@clarkezone.net>
2022-02-18 05:40:50 +0000

    Multi-image build

--- Preview/scripts/buildandpush.sh
+++ Preview/scripts/buildandpush.sh
@@ -1,4 +1,15 @@
 #podman build . -t ghcr.io/clarkezone/jekyllblogpreview:latest
 #podman push ghcr.io/clarkezone/jekyllblogpreview:latest
-podman build . -t registry.dev.clarkezone.dev/jekyllblogpreview:latest
-podman push registry.dev.clarkezone.dev/jekyllblogpreview:latest
+export IMG=previewd
+export VERSION=0.0.1.0
+
+podman build --arch=amd64 -t ${IMG}:${VERSION}.amd64 -f Dockerfile
+podman build --arch=arm64 -t ${IMG}:${VERSION}.arm64 -f Dockerfile
+
+podman manifest create ${IMG}:${VERSION}
+podman manifest add ${IMG}:${VERSION} ${IMG}:${VERSION}.amd64
+podman manifest add ${IMG}:${VERSION} ${IMG}:${VERSION}.arm64
+
+
+#podman build . -t registry.dev.clarkezone.dev/jekyllblogpreview:latest
+#podman push registry.dev.clarkezone.dev/jekyllblogpreview:latest
---
+++ Preview/scripts/create-release-tag.sh
@@ -0,0 +1,2 @@
+git tag -a 0.0.1.0
+git push origin 0.0.1.0
