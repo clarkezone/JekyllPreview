4bf869f9b5ae2460cea0ea135fa825e04b5d4c15
James Clarke <james@clarkezone.net>
2022-04-17 07:11:01 -0700

    update curl test

--- Preview/scripts/webhooktest.sh
+++ Preview/scripts/webhooktest.sh
@@ -1,3 +1,3 @@
 curl -X POST http://0.0.0.0:8090/postreceive \
-   -H 'Content-Type: application/json' -H 'X-GitLab-Event: Push Hook' \
+   -H 'Content-Type: application/json' -H 'X-GitHub-Event: push' \
    -d @webhook.json
